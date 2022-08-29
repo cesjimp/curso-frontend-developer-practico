7de14b1b22f410429bf3d865a9985c41c06b149f
Cesar Jimenez <cesjimp@gmail.com>
2022-08-29 15:04:13 -0500

    Reaccion de menus

--- main.js
+++ main.js
@@ -5,11 +5,22 @@ const mobileMenu = document.querySelector(".mobile-menu")
 const btnShoping = document.querySelector(".navbar-shopping-cart")
 const contCarritoCompras=document.querySelector(".product-detail")
 const contenedorProductos=document.getElementById("contenedor_productos")
+const cuadroDescripcion = document.querySelector(".product-detail2")
+const productDescriptionMenu=document.querySelector(".product-detail2")
+let productosEnPantalla;
+arrayBotones=[]
+
+
+
+
+
+let isProductDescriptionClosed;
 let isMenuDesktopClosed;
 let ismobileMenuClosed;
 let iscontCarritoComprasClosed;
 let arrayListaProductos=[]
 let productoEnLista;
+let productDetail=document.querySelector(".product-detail2")
 
 
 naveEmailOver.addEventListener("click", clickMenuDesktop)
@@ -17,12 +28,17 @@ menuMobile.addEventListener("click", clickMenuMobile)
 btnShoping.addEventListener("click", cickCarritoCompras)
 
 
+
+
+
+
 function clickMenuDesktop()
 {
 
-    if (!iscontCarritoComprasClosed)
+    if (!iscontCarritoComprasClosed || !isProductDescriptionClosed)
     {
         contCarritoCompras.classList.add("inactive")
+        productDescriptionMenu.classList.add("inactive")
         desktopMenu.classList.toggle("inactive");
     }
     else if (isMenuDesktopClosed && iscontCarritoComprasClosed)
@@ -60,14 +76,15 @@ guardarVariables()
 
 function cickCarritoCompras()
 {
-    if (iscontCarritoComprasClosed&&ismobileMenuClosed && isMenuDesktopClosed)
+    if (iscontCarritoComprasClosed&&ismobileMenuClosed && isMenuDesktopClosed && isProductDescriptionClosed)
     {
         contCarritoCompras.classList.toggle("inactive");
     }
         
-    else if (!isMenuDesktopClosed || !ismobileMenuClosed)
+    else if (!isMenuDesktopClosed || !ismobileMenuClosed ||!isProductDescriptionClosed)
     {
         desktopMenu.classList.add("inactive")
+        productDescriptionMenu.classList.add("inactive")
         mobileMenu.classList.add("inactive")
         contCarritoCompras.classList.toggle("inactive");
     }
@@ -81,10 +98,11 @@ function cickCarritoCompras()
 function guardarVariables()
 
 {
+    isProductDescriptionClosed=productDescriptionMenu.classList.contains("inactive")
     isMenuDesktopClosed=desktopMenu.classList.contains("inactive")
     ismobileMenuClosed=mobileMenu.classList.contains("inactive")
     iscontCarritoComprasClosed=contCarritoCompras.classList.contains("inactive")
-    console.log(isMenuDesktopClosed, ismobileMenuClosed, iscontCarritoComprasClosed )
+    console.log(isMenuDesktopClosed, ismobileMenuClosed, iscontCarritoComprasClosed, isProductDescriptionClosed )
 }
 
 
@@ -95,6 +113,7 @@ class productlist
         this.image=image
         this.price=price
         this.item=item
+    
     }
 
 }
@@ -114,30 +133,94 @@ arrayListaProductos.push(bike,televisor,celular,bike2,televisor2,celular2)
 arrayListaProductos.forEach((productlist)=>
 {
     productoEnLista=`
-    <div class="product-card">
+    <div class="product-card" id="product-card${arrayListaProductos.indexOf(productlist)}">
         <img src="${productlist.image}">
         <div class="product-info">
           <div>
-            <p>$ ${productlist.price}</p>
-            <p>${productlist.item}</p>
+            <p class="price">$ ${productlist.price}</p>
+            <p class="item">${productlist.item}</p>
           </div>
           <figure>
-            <img src="./icons/bt_add_to_cart.svg" alt="">
+          <p id="btn_add${arrayListaProductos.indexOf(productlist)}">
+            <img src="./icons/bt_add_to_cart.svg" alt="" >
+            </p>
           </figure>
         </div>
     `
-    contenedorProductos.innerHTML+=productoEnLista
-
+    
+    contenedorProductos.innerHTML     
+    +=productoEnLista
 }
 )
 
-for (producto in arrayListaProductos)
+
+const product_card0=document.getElementById("product-card0")
+product_card0.addEventListener("click",opDescription)
+const product_card1=document.getElementById("product-card1")
+product_card1.addEventListener("click",opDescription)
+const product_card2=document.getElementById("product-card2")
+product_card2.addEventListener("click",opDescription)
+const product_card3=document.getElementById("product-card3")
+product_card3.addEventListener("click",opDescription)
+const product_card4=document.getElementById("product-card4")
+product_card4.addEventListener("click",opDescription)
+const product_card5=document.getElementById("product-card5")
+product_card5.addEventListener("click",opDescription)
+// const product_card6=document.getElementById("product-card6")
+// product_card6.addEventListener("clic",opDescription)
+// const product_card7=document.getElementById("product-card7")
+// product_card7.addEventListener("clic",opDescription)
+// const product_card8=document.getElementById("product-card8")
+// product_card8.addEventListener("clic",opDescription)
+// const product_card9=document.getElementById("product-card9")
+// product_card9.addEventListener("clic",opDescription)
+// const product_card10=document.getElementById("product-card10")
+// product_card10.addEventListener("clic",opDescription)
+
+
+
+
+
+
+
+
+// for (producto in arrayListaProductos)
+// {
+//     console.log(arrayListaProductos[producto].item)
+// }
+
+// for (producto of arrayListaProductos)
+// {
+//     console.log(producto.item)
+// }
+
+
+
+productosEnPantalla= document.getElementsByClassName("product-card");
+
+
+
+function seleccionarItem()
 {
-    console.log(arrayListaProductos[producto].item)
+        console.log (productoSeleccionado)
 }
 
-for (producto of arrayListaProductos)
+function opDescription ()
 {
-    console.log(producto.item)
+      
+    if (!iscontCarritoComprasClosed || !isMenuDesktopClosed)
+    {
+        contCarritoCompras.classList.add("inactive");
+        desktopMenu.classList.add("inactive")
+        productDetail.classList.toggle("inactive")
+    }
+    else
+    {
+        productDetail.classList.toggle("inactive")
+    }  
+    guardarVariables()
 }
+
+
+
 
